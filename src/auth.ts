f83902791727ca63841122dd278866350e6876a5
Riad Mefti <104847379+RiadMefti@users.noreply.github.com>
2025-03-03 06:57:42 -0500

    feat: integrate next-auth for authentication with Google provider

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import type {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import type { NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const config = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google" && profile?.email) {
+        return true;
+      }
+      return false;
+    },
+  },
+} satisfies NextAuthOptions;
+
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, config);
+}
